b58ec899c155b690b10fe10c2907dce105135b21
Aizak <aizakwong@gmail.com>
2024-03-18 18:35:48 +1000

    Made main.java parse the command line arguments into obstacles and create a map with those obstacls.

--- src/main.java
+++ src/main.java
@@ -1,11 +1,45 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import obstacles.*;
+import common.*;
 
 class Main {
     public static void main(String[] args) {
+        // Parse the command line arguments into obstacles
+        // and create a map with those obstacles
         HashMap<String, ArrayList<String>> parsedArgs = parseArgs(args);
-        System.out.println(parsedArgs);
+        ArrayList<Obstacle> obstacles = parseObstacles(parsedArgs);
+        Map map = new Map(obstacles);
+
+        // Parse the start and target locations
+        String startArg = stripParentheses(parsedArgs.get("-start").get(0));
+        String targetArg = stripParentheses(parsedArgs.get("-target").get(0));
+        Location start = Location.parse(startArg);
+        Location target = Location.parse(targetArg);
+
+        // Show the map
+        System.out.println(map.getSolvedMap(start, target));
+    }
+
+    /**
+     * Parses the command line arguments into a HashMap of arguments
+     * @param args The command line arguments
+     * @return A HashMap of arguments
+     */
+    private static HashMap<String, ArrayList<String>> parseArgs(String[] args) {
+        HashMap<String, ArrayList<String>> parsedArgs = new HashMap<>();
+        ArrayList<String> argValues = null;
+        for (String arg : args) {
+            if (arg.startsWith("-")) {
+                argValues = new ArrayList<>();
+                parsedArgs.put(arg, argValues);
+                continue;
+            }
+            if (argValues != null) {
+                argValues.add(arg);
+            }
+        }
+        return parsedArgs;
     }
 
     /**
